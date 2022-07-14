09502a98d367caf9fc11651050b83db9252837a7
Luis Miguel <lmavendano1597@gmail.com>
2022-07-14 16:37:38 -0500

    correcion ,

--- peliculas.sql
+++ peliculas.sql
@@ -18,14 +18,14 @@ INSERT INTO peliculas (id, titulo, calificacion) VALUES
 (7, 'Iron Man', 4),
 (8, 'Mad Max: Furia en la carretera', 3),
 (9, 'Moonlight', 4),
-(10, 'Parásitos', 1)
-(11, 'Tiburón', 3)
-(12, 'Doce hombres sin piedad', 3)
-(13, 'Días del cielo', 4)
-(14, 'Los dies negritos', 5)
-(15, 'Los Cazafantasmas', 2)
-(16, 'Ladrón que roba ladrón', 1)
-(17, 'Muerte entre las flores', 3)
-(18, 'Terminator', 2)
-(19, 'Black Panther', 2)
+(10, 'Parásitos', 1),
+(11, 'Tiburón', 3),
+(12, 'Doce hombres sin piedad', 3),
+(13, 'Días del cielo', 4),
+(14, 'Los dies negritos', 5),
+(15, 'Los Cazafantasmas', 2),
+(16, 'Ladrón que roba ladrón', 1),
+(17, 'Muerte entre las flores', 3),
+(18, 'Terminator', 2),
+(19, 'Black Panther', 2),
 (20, 'Efecto mariposa', 5)
