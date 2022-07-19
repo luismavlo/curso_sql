6981bcf2f1fac0a122d919df378fa86a20e168b5
Luis Miguel <lmavendano1597@gmail.com>
2022-07-19 09:32:59 -0500

    table books add

--- libros.sql
+++ libros.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS libros (
   titulo varchar(80) NOT NULL,
   calificacion int NOT NULL,
   paginas int DEFAULT NULL,
-  precio int NOT NULL
+  precio int NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -20,7 +20,7 @@ INSERT INTO libros (id, titulo, calificacion, paginas, precio) VALUES
 (10, 'La voz de tu alma', 1, 416, 10),
 (11, 'Mi gran libro de dinosaurios', 2, 100, 10),
 (12, 'They both die at the end', 3, 345, 13),
-(13, 'El laberinto de los espiritus', 5, 928, 25)
+(13, 'El laberinto de los espiritus', 5, 928, 25),
 (15, 'El nombre del viento', 5, 871, 20),
 (16, 'Sigue mi voz', 1, 336, 15),
 (17, 'Mi lucha', 4, 948, 30),
@@ -30,4 +30,4 @@ INSERT INTO libros (id, titulo, calificacion, paginas, precio) VALUES
 (21, 'El jugador', 3, NULL, 18),
 (22, 'El extranjero', 3, NULL, 10),
 (23, 'La voragine', 2, NULL, 15),
-(23, 'De la tierra a la luna', 4, NULL, 18)
+(24, 'De la tierra a la luna', 4, NULL, 18)
